f3ea5df18d861aab7eab7e236f66cc4f3592db05
nverno <noah.v.peart@gmail.com>
2020-12-07 13:05:10 -0800

    Add initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see
+  # http://guides.rubyonrails.org/routing.html
+  root to: 'static_pages#root'
+
+  namespace :api, defaults: { format: :json } do
+    resource :user, only: %i[create update destroy show]
+    resource :session, only: %i[create update destroy]
+    resources :assets, only: %i[index create update destroy]
+    resources :transactions, only: %i[index create update]
+    resources :lists, only: %i[index show create update delete]
+  end
 end
